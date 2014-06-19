8ffe4b43bb406b96eb51653234648a2e53bd44e8
Dmitry <dmitry@Dmitrys-Mac-Pro.local>
2014-06-19 16:41:47 +0300

    Add new ViewController

---
+++ HelloWorld001/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld001
+//
+//  Created by Dmitry on 6/19/14.
+//  Copyright (c) 2014 Dmitry org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HelloWorld001/ViewController.m
@@ -0,0 +1,44 @@
+//
+//  ViewController.m
+//  HelloWorld001
+//
+//  Created by Dmitry on 6/19/14.
+//  Copyright (c) 2014 Dmitry org. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self)
+    {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)logFunction
+{
+    NSLog(@"My log string");
+}
+
+@end
