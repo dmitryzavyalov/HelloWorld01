70e72faa36cd9556c68c907b3833a9f7c6788ca7
Dmitry <dmitry@Dmitrys-Mac-Pro.local>
2014-06-19 16:36:02 +0300

    Initial commit

---
+++ HelloWorld001/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld001
+//
+//  Created by Dmitry on 6/19/14.
+//  Copyright (c) 2014 Dmitry org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ README.md
@@ -0,0 +1,1 @@
+This is HelloWorld001 test project
